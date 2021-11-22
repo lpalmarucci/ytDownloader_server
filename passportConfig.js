b0fe97ea9c9ad545558f543a389aca94032f72c6
Luca Palmarucci <lpalmarucci@gmail.com>
2021-11-22 16:28:20 +0100

    refactoring + gestione console.log con parametro opzionale

--- passportConfig.js
+++ passportConfig.js
@@ -2,17 +2,18 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt')
 
 
-function initialize(passport, getUserByUsername, getUserById) {
+function initialize(passport, getUserByUsername, debug = false) {
     const authenticateUser = async (username, password, done) => {
+        if (debug) console.log(`username --> ${username}\n password --> ${password}`);
         const user = await getUserByUsername(username);
-        if (user?.error) {
-            return done(null, false, { message: 'User not found! Please register before signing in' })
+        if (debug) console.log(`user --> ${JSON.stringify(user)}`);
+        if (user?.errorMessage) {
+            return done(null, false, { message: user.errorMessage })
         }
         if (username === null) {
             return done(null, false, { message: 'User not valid' })
         }
         try {
-
             if (await bcrypt.compare(password, user.password)) {
                 return done(null, user);
             } else {
